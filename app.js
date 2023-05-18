4206bf8a7c44c41ff3a7c6b24213bfa10df664a6
mohamedshamil7 <mohamedshamil0507@gmail.com>
2023-05-18 20:28:39 +0530

    basic structure changes and initial connection of sequelize bin folder deleted port connection changed to app.js basic connection of sequilize done

--- app.js
+++ app.js
@@ -3,11 +3,76 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
+var http = require('http');
+var debug = require('debug')('mysqlnode:server');
 var usersRouter = require('./routes/users');
+const { Sequelize } = require('sequelize');
+require("dotenv").config()
+
+
+const sequelize = new Sequelize(process.env.dbName, process.env.username, process.env.password, {
+  dialect:'mysql' 
+})
+const  authenticate_sequelize =async()=>{
+try {
+    
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+}
+authenticate_sequelize()
 
 var app = express();
+
+
+var server = http.createServer(app);
+// var port = normalizePort(process.env.PORT || '3000');
+var port= '3000'
+// app.set('port', port);
+
+
+
+
+server.listen(port,(()=>{
+  console.log('Server is running on port '+port);
+}));
+server.on('error', onError);
+server.on('listening', onListening);
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +84,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use('/', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
